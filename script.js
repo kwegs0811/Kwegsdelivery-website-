b1fc65aa629ecd6fea3cfaccd60493dd615fc440
KWEGS TONICS <francisdavid0811@gmail.com>
2025-05-17 17:25:45 +0300

    Update main.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCLv_JPPpLeQFI9dFZxNwQXBMEIXCCushA",
@@ -10,11 +8,9 @@ const firebaseConfig = {
   messagingSenderId: "288377432780",
   appId: "1:288377432780:web:904bb112a587b1393fb069"
 };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-function sendDestination() {
-  const dest = document.getElementById("destination").value;
-  set(ref(db, "command"), dest);
+function sendCommand(cmd) {
+  db.ref('car/command').set(cmd);
 }
